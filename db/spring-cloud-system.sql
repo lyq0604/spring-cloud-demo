55b3d7194fd47fb6798d24149879f33ee00701ec
lyq0604 <848985477@qq.com>
2019-09-26 17:44:50 +0800

    fix: db文件

--- db/spring-cloud-system.sql
+++ db/spring-cloud-system.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-09-24 17:29:05
+Date: 2019-09-26 17:44:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,13 +25,18 @@ CREATE TABLE `base_menu` (
   `menu_path` varchar(255) DEFAULT NULL,
   `menu_description` varchar(255) DEFAULT NULL,
   `parent_id` varchar(36) DEFAULT NULL,
+  `menu_icon` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of base_menu
 -- ----------------------------
-INSERT INTO `base_menu` VALUES ('1', '用户管理', '/user', '用户管理相关', '-1');
+INSERT INTO `base_menu` VALUES ('1', '系统管理', '/system', '系统管理相关', '-1', 'table');
+INSERT INTO `base_menu` VALUES ('2', '博客管理', '/blog', '博客管理相关', '-1', 'table');
+INSERT INTO `base_menu` VALUES ('3', '用户管理', '/user', '用户管理', '1', 'table');
+INSERT INTO `base_menu` VALUES ('4', '角色管理', '/role', '角色管理', '1', 'table');
+INSERT INTO `base_menu` VALUES ('5', '字典管理', '/dict', '字典管理', '1', 'table');
 
 -- ----------------------------
 -- Table structure for base_operation
@@ -64,7 +69,7 @@ CREATE TABLE `base_permission` (
 -- ----------------------------
 -- Records of base_permission
 -- ----------------------------
-INSERT INTO `base_permission` VALUES ('1', '用户管理菜单', 'PER_MENU_USER', '用户管理菜单权限', 'MENU');
+INSERT INTO `base_permission` VALUES ('1', '编辑用户', 'PER_USER_EDIT', '编辑用户操作权限', 'OPERA');
 INSERT INTO `base_permission` VALUES ('2', '新增用户', 'PER_USER_ADD', '新增用户操作权限', 'OPERA');
 
 -- ----------------------------
@@ -74,14 +79,15 @@ DROP TABLE IF EXISTS `base_role`;
 CREATE TABLE `base_role` (
   `id` varchar(36) NOT NULL,
   `role_name` varchar(20) DEFAULT NULL,
+  `role_code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of base_role
 -- ----------------------------
-INSERT INTO `base_role` VALUES ('1', 'ROLE_ADMIN');
-INSERT INTO `base_role` VALUES ('2', 'ROLE_USER');
+INSERT INTO `base_role` VALUES ('1', '管理员', 'ROLE_ADMIN');
+INSERT INTO `base_role` VALUES ('2', '普通用户', 'ROLE_USER');
 
 -- ----------------------------
 -- Table structure for base_user
@@ -102,6 +108,26 @@ INSERT INTO `base_user` VALUES ('1', '测试用户', 'test', 'test');
 INSERT INTO `base_user` VALUES ('2', '系统管理员', 'admin', 'admin');
 
 -- ----------------------------
+-- Table structure for rl_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `rl_role_menu`;
+CREATE TABLE `rl_role_menu` (
+  `id` varchar(36) NOT NULL,
+  `role_id` varchar(36) NOT NULL,
+  `menu_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of rl_role_menu
+-- ----------------------------
+INSERT INTO `rl_role_menu` VALUES ('1', '1', '1');
+INSERT INTO `rl_role_menu` VALUES ('2', '1', '2');
+INSERT INTO `rl_role_menu` VALUES ('3', '1', '3');
+INSERT INTO `rl_role_menu` VALUES ('4', '1', '4');
+INSERT INTO `rl_role_menu` VALUES ('5', '1', '5');
+
+-- ----------------------------
 -- Table structure for rl_role_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `rl_role_permission`;
@@ -116,22 +142,7 @@ CREATE TABLE `rl_role_permission` (
 -- Records of rl_role_permission
 -- ----------------------------
 INSERT INTO `rl_role_permission` VALUES ('1', '1', '1');
-
--- ----------------------------
--- Table structure for rl_user_permission
--- ----------------------------
-DROP TABLE IF EXISTS `rl_user_permission`;
-CREATE TABLE `rl_user_permission` (
-  `id` varchar(36) NOT NULL,
-  `user_id` varchar(36) NOT NULL,
-  `permission_id` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of rl_user_permission
--- ----------------------------
-INSERT INTO `rl_user_permission` VALUES ('1', '1', '2');
+INSERT INTO `rl_role_permission` VALUES ('2', '1', '2');
 
 -- ----------------------------
 -- Table structure for rl_user_role
